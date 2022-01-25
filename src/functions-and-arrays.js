df4ff4662c02ae4446ad004baf5b24177dca2820
Noelia Parada <noeliacastrelo@gmail.com>
2022-01-25 17:24:49 +0100

    just starting

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,6 +1,8 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
-
+function maxOfTwoNumbers(num1, num2) {}
+if (num1 > num2){
+  return num1
+}else if (num2 > num1)
 
 
 // Iteration #2: Find longest word
